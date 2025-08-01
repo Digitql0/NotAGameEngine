6684528dae174ac7b7d1dc4ff6e62cb6b04156a4
Digitql0 <imonkix@gmail.com>
2025-08-01 13:48:33 +0200

    finished basic drawing library necessities

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@ int main() {
 
 	std::cout << "Making Window..." << std::endl;
 	MakeWindow(800, 600, "Lol how did i do this");
+	std::cout << "This is not real" << std::endl;
+
+	Rectangle a = {250, 0, 100, 100};
+	Rectangle b = {500, 0, 100, 100};
+	Rectangle c = {0, 0, 100, 100};
+
+	Vec2 a_velocity = {1, 0};
 
 	while(!WindowShouldClose()) {
 		//std::cout << "Beginning Drawing..." << std::endl;
@@ -15,7 +22,15 @@ int main() {
 		ClearBackground(255, 255, 255);
 
 		//std::cout << "Drawing Rectangle..." << std::endl;
-		drawRectangle(10, 10, 100, 100, 255, 0, 0);
+		drawRectangle(a.x, a.y, a.w, a.h, 255, 0, 0);
+		drawRectangle(b.x, b.y, b.w, b.h, 0, 255, 255);
+		drawRectangle(c.x, c.y, c.w, c.h, 0, 0, 255);
+		a.x += a_velocity.x;
+		a.y += a_velocity.y;
+
+		if (CheckCollisionRectangles(a, b) || CheckCollisionRectangles(a, c)) {
+			a_velocity = getScaledVec(a_velocity, -1);
+		}
 
 		//std::cout << "Ending Drawing..." << std::endl;
 		EndDrawing();
